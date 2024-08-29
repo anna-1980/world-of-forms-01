7b7f9a4be085f96a0ee64a9bc09a674e9cde10b4
Anna Zdyb <89396456+anna-1980@users.noreply.github.com>
2024-08-29 08:57:39 +0200

    refreshing the reactive forms

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from "../forms/reactive-forms/login/login.component";
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
